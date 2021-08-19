4a3ce76e86ac47306e0b25d4ea5738e9fc07c258
pragyanaryal <pragyan.aryal@wesionary.team>
2021-08-19 10:07:46 +0545

    fnished: quiz app

---
+++ complete_flutter/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:complete_flutter/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Transaction> transactions = [
+    Transaction(id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(id: 't2', title: 'Weekly Groceries', amount: 16.53, date: DateTime.now())
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Complete App'),
+        ),
+        body: Column(
+          children: [
+            Card(
+              color: Colors.lightBlue,
+              child: Container(
+                width: double.infinity,
+                child: Text('Chart'),
+              ),
+              elevation: 5,
+            ),
+            Column(
+                children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList()),
+          ],
+        ),
+      ),
+    );
+  }
+}
